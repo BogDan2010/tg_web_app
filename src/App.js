84873a14d845542123270baeb16241006c8ac0af
ba77595 <ba77595@gmail.com>
2025-02-24 17:02:23 +0300

    Modified.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 
 import './App.css';
 import Header from './components/Header/Header';
+import { useTelegram } from './hooks/useTelegram';
 
-const tg = window.Telegram.WebApp;
 function App() {
+	const { tg } = useTelegram();
 	useEffect(() => {
 		tg.ready();
 	}, []);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,15 @@
 import React from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
 import Button from '../../ui/Button/Button';
 import styles from './Header.module.scss';
 
 const Header = () => {
-	const tg = window.Telegram.WebApp;
+	const { user, onClose } = useTelegram();
 
-	const onClose = () => {
-		console.log('click');
-
-		tg.close();
-	};
 	return (
 		<div className={styles.container}>
 			<Button onClick={onClose}>Закрыть</Button>
-			<span className={styles.username}>
-				{tg.initDataUnsafe?.user?.username}
-			</span>
+			<span className={styles.username}>{user?.username}</span>
 		</div>
 	);
 };
