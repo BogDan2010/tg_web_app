fdb2102d8770a15740aa7fd1d8b4c51e35feff12
ba77595 <ba77595@gmail.com>
2025-02-27 16:04:02 +0300

    Mod 6

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useTelegram } from '../../hooks/useTelegram';
-import Button from '../Button/Button';
+// import { useTelegram } from '../../hooks/useTelegram';
+// import Button from '../Button/Button';
 import styles from './Header.module.scss';
 
 const Header = () => {
-	const { user, onClose } = useTelegram();
+	// const { user, onClose } = useTelegram();
 
 	return (
 		<div className={styles.container}>
-			<Button onClick={onClose}>Закрыть</Button>
+			{/* <Button onClick={onClose}>Закрыть</Button> */}
 			<span className={styles.username}>{user?.username}</span>
 		</div>
 	);
