f2af54eb11f0f4abe313cd9188ac112d720561c6
ba77595 <ba77595@gmail.com>
2025-03-03 15:45:06 +0300

    Mod

--- src/components/Backet/Backet.js
+++ src/components/Backet/Backet.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import { useLocation } from 'react-router-dom';
+import { useTelegram } from '../../hooks/useTelegram';
 import Header from '../Header/Header';
 import styles from './Backet.module.scss';
 
 const Backet = () => {
 	const location = useLocation();
 	const orderData = location?.state?.data;
+	const { tg } = useTelegram();
+	const onSendData = useCallback(async () => {
+		// const data = {
+		// 	city,
+		// 	street,
+		// 	subject,
+		// };
+
+		await fetch('https://81eb-68-183-154-121.ngrok-free.app/web-data', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(orderData),
+		});
+	}, [orderData]);
+
+	useEffect(() => {
+		tg.onEvent('mainButtonClicked', onSendData);
+		return () => {
+			tg.offEvent('mainButtonClicked', onSendData);
+		};
+	}, [onSendData]);
+
+	useEffect(() => {
+		tg.MainButton.setParams({
+			text: 'Отпрвить данные',
+		});
+	}, []);
+
 	return (
 		<div className={styles.container}>
 			<Header title={'Ваш заказ'} titleButton={'Редактировать'} />
