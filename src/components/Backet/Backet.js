b8ace8a3b87f24d30ae8a82284fdac533b5d64c9
ba77595 <ba77595@gmail.com>
2025-03-04 13:06:34 +0300

    new mod

--- src/components/Backet/Backet.js
+++ src/components/Backet/Backet.js
@@ -21,7 +21,7 @@ const Backet = () => {
 		// 	subject,
 		// };
 
-		await fetch('https://d7f0-68-183-154-121.ngrok-free.app/web-data', {
+		await fetch('https://e156-68-183-154-121.ngrok-free.app/web-data', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
